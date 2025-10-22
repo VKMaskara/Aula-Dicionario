e077f5a2e957287ff41d5b31546360eb456d5d50
Vitor Kauê <rodriguesvitorkaue@gmail.com>
2025-10-21 22:06:51 -0300

    Aula referente ao dia 21/10/2025 - Aprendendo Dicioário

---
+++ aula-dicinario.py
@@ -0,0 +1,59 @@
+'''lista = {'João', 'Maria', 'Pedro'}
+lista = dict() cria um dicionário vazio'''
+lista = {'nome': 'Pedro', 'idade': 25,}
+'''print(lista['nome'])
+print(lista['idade'])'''                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
+
+print("==" * 20)                                                          
+print(f"{lista['nome']} tem {lista['idade']} anos.")
+print("==" * 20)     
+
+lista['sexo'] = 'M'  # Adiciona um novo par chave-valor ao dicionário
+print(lista)
+
+print("==" * 20)
+del lista['idade']  # Remove o par chave-valor com a chave 'idade'
+print(lista)
+
+print("==" * 20)
+
+filme = {'nome do filme': 'Veloses e Furiosos', 
+         'Ano': 2001, 
+         'Diretor': ' Rob Cohen'}
+
+print(f"O filme {filme['nome do filme']} foi lançado em {filme['Ano']} e dirigido por {filme['Diretor']}.")
+
+print(filme.values())  # Exibe todos os valores do dicionário
+print(filme.keys())    # Exibe todas as chaves do dicionário
+print(filme.items())   # Exibe todos os pares chave-valor do dicionário
+
+print("==" * 20)
+for k, v in filme.items():
+    print(f"O {k} é {v}")
+
+print("==" * 20)
+
+pessoas = {'nome': 'Ana', 'idade': 30, 'sexo': 'F'}
+#print(pessoas[0]) # Isso causará um erro, pois dicionários não suportam indexação por posição
+print(f'O {pessoas['nome']} tem {pessoas["idade"]} anos')  # Acessa o valor associado à chave 'nome'
+
+print("==" * 20)    
+
+del pessoas['sexo']  # Remove o par chave-valor com a chave 'sexo'
+
+pessoas['nome'] = 'Maria'  # Atualiza o valor associado à chave 'nome'
+pessoas['idade'] = 28      # Atualiza o valor associado à chave 'idade'
+print(pessoas)  # Exibe o dicionário atualizado
+
+pessoas['peso'] = 65.5  # Adiciona um novo par chave-valor ao dicionário
+print(pessoas)  # Exibe o dicionário atualizado
+ 
+brasil =[]
+estados1 = {'nome': 'São Paulo', 'sigla': 'SP'}
+estados2 = {'nome': 'Rio de Janeiro', 'sigla': 'RJ'}
+brasil.append(estados1)
+brasil.append(estados2)
+print(brasil)
+print(brasil[0])  # Acessa o nome do primeiro estado na lista
+print(brasil[1]["nome"])  # Acessa a sigla do segundo estado na lista
