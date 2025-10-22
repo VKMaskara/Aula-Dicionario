57271e06174fb199691a5ba84f3dcf6ae1e95c7c
Vitor Kauê <rodriguesvitorkaue@gmail.com>
2025-10-22 20:26:40 -0300

    finalizando aula dicionario data: 22/10/20225

--- aula-dicinario.py
+++ aula-dicinario.py
@@ -2,7 +2,7 @@
 lista = dict() cria um dicionário vazio'''
 lista = {'nome': 'Pedro', 'idade': 25,}
 '''print(lista['nome'])
-print(lista['idade'])'''                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+print(lista['idade'])                                                             
 
 
 print("==" * 20)                                                          
@@ -57,3 +57,48 @@ brasil.append(estados2)
 print(brasil)
 print(brasil[0])  # Acessa o nome do primeiro estado na lista
 print(brasil[1]["nome"])  # Acessa a sigla do segundo estado na lista
+
+
+print("==" * 20)    
+
+
+materia = dict()
+curso = list()
+
+for c in range (0,3):
+    materia['sigla'] = str(input("Digite a Sigla da Matéria: "))
+    materia["nome"] = str(input("Digite o nome da Matéria: "))
+    curso.append(materia.copy())
+print(curso)
+
+for m in curso:
+    for k, v , in m.items():
+        print(f"O Campo {k} tem valor {v}")
+
+lista = []
+dic=dict()
+
+nome = input("Digite: ")
+
+idade = int(input("Digite idade: "))
+
+lista.append(nome)
+lista.append(idade)
+print("==" * 20)  
+
+dic = {"nome": lista[0], "idade": lista[1]}
+print(lista)
+print(dic["nome"])
+'''
+
+dados = {
+    'Crossfox': {'km': 35000, 'ano': 2005},
+     'DS5': {'km': 17000, 'ano': 2015},
+     'Fusca': {'km': 130000, 'ano': 1979}, 'Jetta': {'km': 56000, 'ano': 2011},
+    'Passat': {'km': 62000, 'ano': 1999 }
+    }
+
+for item in dados.items():
+    print(item)
+for item in dados.items():
+    print(item[1]['ano'])
